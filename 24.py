586b1ded3092963fd0793238990fa5ca6187993d
samuelbasnet <samueallal743@gmail.com>
2024-12-21 21:07:56 +0545

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,17 @@
+a = True
+b = True
+c = True
+d = True
+
+print(c)
+print(d)
+print(not a)
+print(not b)
+print(not c)
+print(not d)
+print(a and b)
+print(a or b)
+print(a and b or c)
+print(not a or b or c)
+print(not a or not b or not c)
+print(not(not a or not b or not c))
