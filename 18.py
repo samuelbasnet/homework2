a6e06a9742f74431e811c725cbc0968116ed941c
samuelbasnet <samueallal743@gmail.com>
2024-12-21 21:03:02 +0545

    Create 18.py

---
+++ 18.py
@@ -0,0 +1,10 @@
+city = input("Enter the city: ").lower()
+
+if city == "delhi":
+    print("The monument is Red Fort.")
+elif city == "agra":
+    print("The monument is Taj Mahal.")
+elif city == "jaipur":
+    print("The monument is Jal Mahal.")
+else:
+    print("No monument information available for this city.")
