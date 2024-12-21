5fd91c14d7f2a88726d1537a47db2384daa42544
samuelbasnet <samueallal743@gmail.com>
2024-12-21 20:47:59 +0545

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,6 @@
+first_number= int(input("Enter first number: "))
+second_number=int(input("Enter second number: "))
+first_number=first_number^second_number
+second_number=first_number^second_number
+first_number=first_number^second_number
+print(first_number,second_number)
