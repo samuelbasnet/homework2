f8c977b1ab10679895a761d3799be8cfb0080c0b
samuelbasnet <samueallal743@gmail.com>
2024-12-21 21:03:37 +0545

    Create 19.py

---
+++ 19.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number: "))
+
+if number % 2 == 0 and number % 3 == 0:
+    print("The number is divisible by both 2 and 3.")
+else:
+    print("The number is not divisible by both 2 and 3.")
